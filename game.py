38278b395fa0bb63c7e6cf94ffa60788c00422c6
Lin <highca0709@gmail.com>
2026-04-09 15:05:15 +0800

    新增判斷勝負功能

--- game.py
+++ game.py
@@ -9,6 +9,20 @@ def draw_board():
     print("-+-+-")
     print(board[6] + "|" + board[7] + "|" + board[8])
 
+def check_winner():
+    # 橫的
+    if board[0] == board[1] == board[2] != " ": return board[0]
+    if board[3] == board[4] == board[5] != " ": return board[3]
+    if board[6] == board[7] == board[8] != " ": return board[6]
+    # 直的
+    if board[0] == board[3] == board[6] != " ": return board[0]
+    if board[1] == board[4] == board[7] != " ": return board[1]
+    if board[2] == board[5] == board[8] != " ": return board[2]
+    # 斜的
+    if board[0] == board[4] == board[8] != " ": return board[0]
+    if board[2] == board[4] == board[6] != " ": return board[2]
+    return None
+    
 def play():
     current_player = "X"
     for turn in range(9):
@@ -16,10 +30,16 @@ def play():
         print("輪到 " + current_player + " 下棋")
         position = int(input("選擇位置 (1-9)：")) - 1
         board[position] = current_player
+        winner = check_winner()
+        if winner:
+            draw_board()
+            print(winner + " 獲勝！🎉")
+            return
         if current_player == "X":
             current_player = "O"
         else:
             current_player = "X"
     draw_board()
+    print("平局！")
 
 play()
